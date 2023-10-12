68cb4dc65fa8b9296f4c7b3d31ac51f8324211b5
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-10-13 00:06:00 +0530

    Initial Commit

---
+++ Word Puzzler App/Word_Puzzler_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_Puzzler_AppApp.swift
+//  Word Puzzler App
+//
+//  Created by Shubham on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Word_Puzzler_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
